2add9dac157e647c5e8b099dba6d5b07806fa935
vnovikov <vynovikov@mail.ru>
2026-09-07 13:30:48 +0300

    Main

---
+++ src/com/learn/Main.java
@@ -0,0 +1,12 @@
+package com.learn;
+
+import java.lang.Math;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        String aa = "dd";
+        int intVal = Math.toIntExact(0x80000000);
+        boolean nnn = true;  //  Комбинация клавиш: Ctrl + Alt + V
+    }
+}
